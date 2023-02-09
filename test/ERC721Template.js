374859f68184450c9ecc63ce4cc589980b08be3a
hoseadevops <hoseadevops@gmail.com>
2023-02-09 12:00:34 +0800

    test

--- test/ERC721Template.js
+++ test/ERC721Template.js
@@ -5,63 +5,14 @@ const {
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
-const fs = require('fs');
-const keccak256 = require('keccak256')
-const { MerkleTree } = require('merkletreejs')
+const { deployERC721Template } = require('./common.js')
 
 describe("ERC721Template", function () {
   
-  async function player() {
-    const json = JSON.parse(fs.readFileSync('./test/player.json', { encoding: 'utf8' }))
-    if (typeof json !== 'object') throw new Error('Invalid JSON')
-    return Object.entries(json);
-  }
-
   async function getMaxTokenID(nft) {
     let maxTokenID = await nft.maxTokenID();
-    console.log("        ✔", { tokenID : ethers.BigNumber.from(maxTokenID).toString() });
+    console.log("        ✔", { maxTokenID : ethers.BigNumber.from(maxTokenID).toString() });
     return maxTokenID;
-  }
-
-  async function deployERC721Template() {
-    // mock player
-    let players = await player();   
-    
-    // merkle tree 
-    const leaves = players.map(( player, _) => {
-        // to  tokenID(101 ~ 199)   uri
-        return ethers.utils.solidityKeccak256(['address', 'uint256', 'string'], [player[0],  player[1]+100, ""]);
-    })
-    const tree = new MerkleTree(leaves, keccak256, { sort: true })
-    const root = tree.getHexRoot()
-    
-    let testUser = players[10];
-    const leaf = ethers.utils.solidityKeccak256(['address', 'uint256', 'string'], [testUser[0], testUser[1]+100, ""]);
-    const proof = tree.getHexProof(leaf)
-
-    // construct nft
-    let config = {
-      name : "name",
-      symbol : 'symbol',
-      prefixURI : 'https://ipfs.io/ipfs/',
-      min : 101,
-      max : 199 
-    }
-    const [owner, admin, operator, bob, sam, tom] = await ethers.getSigners();
-
-    const ERC721Template = await ethers.getContractFactory("ERC721Template");
-    const nft = await ERC721Template.deploy (
-      admin.address, 
-      operator.address, 
-      root, 
-      config.name, 
-      config.symbol, 
-      config.prefixURI,
-      config.min,
-      config.max
-    );
-    
-    return { nft, config, proof, testUser, bob, operator, sam, tom, root, owner, admin};
   }
 
   describe("get", function () {
@@ -76,6 +27,7 @@ describe("ERC721Template", function () {
 
   describe("mint", function () {
     describe("user mint", function () {
+
       it("Should mint token by merkle tree.", async function () {
         const { nft, config, proof, testUser, bob } = await loadFixture(deployERC721Template);
 
@@ -91,33 +43,35 @@ describe("ERC721Template", function () {
           test.proof
         )
         expect(await nft.ownerOf(test.tokenID)).to.be.eq(test.user);
-
         await getMaxTokenID(nft);
       });
 
-      it("Should mint token by merkle self.", async function () {
+      it("Should mint token by self.", async function () {
         const { nft, config, proof, testUser, bob } = await loadFixture(deployERC721Template);
         await nft.connect(bob).selfMint("")
-      
         let tokenID = await getMaxTokenID(nft);
-
         expect(await nft.ownerOf(tokenID)).to.be.eq(bob.address);
+        await getMaxTokenID(nft);
       });
 
-      it("Should mint token by merkle owner.", async function () {
+      it("Should mint token by owner.", async function () {
         const { nft, config, proof, testUser, bob, operator, sam} = await loadFixture(deployERC721Template);
-        let tokenID  = 102;
+        let tokenID  = 102; // 101 ~ 199
         await expect(nft.connect(operator).ownerMint(sam.address, tokenID, "")).to.be.revertedWith("Reserved");
       
         tokenID  = 88;
         await nft.connect(operator).ownerMint(sam.address, tokenID, "");
         expect(await nft.ownerOf(tokenID)).to.be.eq(sam.address);
-
         await getMaxTokenID(nft);
 
         tokenID  = 250;
         await nft.connect(operator).ownerMint(sam.address, tokenID, "");
         expect(await nft.ownerOf(tokenID)).to.be.eq(sam.address);
+        await getMaxTokenID(nft);
+
+        tokenID  = [300, 322, 388];
+        await nft.connect(operator).ownerBatchMint(sam.address, tokenID, ["", "", ""]);
+        expect(await nft.ownerOf(tokenID[2])).to.be.eq(sam.address);
         await getMaxTokenID(nft);
       });
 
